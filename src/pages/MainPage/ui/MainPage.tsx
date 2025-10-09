02c70d08de06699b3dd94f1967da36b436637bb3
gmtdn <failgim@gmail.com>
2025-10-09 21:15:55 +0300

    add headless ui, add flexbox, component

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Input } from 'shared/ui/Input/Input';
-import { ListBox } from 'shared/ui/ListBox/ListBox';
-import { HStack } from 'shared/ui/Stack';
 import { Page } from 'widgets/Page/Page';
 
 const MainPage = () => {
@@ -16,25 +13,6 @@ const MainPage = () => {
     return (
         <Page>
             {t('Главная страница')}
-            <HStack>
-                <div>text 1</div>
-                <ListBox
-                    defaultValue="Выберите значение"
-                    onChange={(value: string) => {}}
-                    value={undefined}
-                    items={[
-                        { value: '1', content: '123' },
-                        { value: '2', content: 'abc', disabled: true },
-                        { value: '13', content: 'bbb' },
-                    ]}
-                />
-            </HStack>
-            <div>text</div>
-            <div>text</div>
-            <div>text</div>
-            <div>text</div>
-            <div>text</div>
-            <div>text</div>
         </Page>
     );
 };
