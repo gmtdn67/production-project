147713def27dd263b2aa3236662d19291977e78c
gmtdn <failgim@gmail.com>
2025-11-01 23:58:01 +0300

    add star rating, rating entity and rating card 2

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page/Page';
-import { RatingCard } from '@/entities/Rating';
 
 const MainPage = () => {
     const { t } = useTranslation();
@@ -14,11 +13,6 @@ const MainPage = () => {
     return (
         <Page>
             {t('Главная страница')}
-            <RatingCard 
-                title={'Как вам статья?'}
-                feedbackTitle={'Оставьте отзыв о статье'}
-                hasFeedback={true}
-                />
         </Page>
     );
 };
