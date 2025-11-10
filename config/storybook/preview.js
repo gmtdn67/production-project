ed6b329727a92a0d8a17abd3064d5e95cb9852ce
gmtdn <failgim@gmail.com>
2025-11-10 21:16:03 +0300

    add storybook theme addon

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -13,7 +13,15 @@ export const parameters = {
             date: /Date$/,
         },
     },
-    layout: 'fullscreen'
+    layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            {name: 'light', class: Theme.LIGHT, color: '#ffffff'},
+            {name: 'dark', class: Theme.DARK, color: '#090949'},
+            {name: 'orange', class: Theme.ORANGE, color: '#bd5012'},
+        ],
+    },
 };
 
 addDecorator(StyleDecorator);
