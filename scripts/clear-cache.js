a6ab86c559af915123c21e0aafb3c25d1494b522
gmtdn <failgim@gmail.com>
2025-11-11 11:03:53 +0300

    update webpack config, add babel cache, cache cleaner

---
+++ scripts/clear-cache.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+async function deleteCache() {
+    const cachePath = '.\\node_modules\\.cache';
+
+    try {
+        await fs.rm(cachePath, { recursive: true, force: true });
+        console.log('Cache deleted successfully');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log('Cahce does not exist');
+        } else {
+            console.error('Error deleting cache:', err);
+        }
+    }
+}
+
+deleteCache();
