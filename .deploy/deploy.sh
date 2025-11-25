783fc7aa503823db09d1427c23e8f75c9e3f3397
gmtdn <failgim@gmail.com>
2025-11-25 21:54:18 +0300

    fix deploy script

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,4 +1,4 @@
 cd ~/production-project
 npm run build:prod
 rm -rf /var/www/production-project
-mv ~/production-project/build /var/www/production_project/html
+mv ~/production-project/build /var/www/production-project/html
