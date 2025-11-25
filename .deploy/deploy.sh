8079b861e63e2d3d572f2f41e376d8bac9cea39e
gmtdn <failgim@gmail.com>
2025-11-25 21:56:27 +0300

    fix deploy script 2

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,4 +1,5 @@
 cd ~/production-project
 npm run build:prod
-rm -rf /var/www/production-project
-mv ~/production-project/build /var/www/production-project/html
+
+rm -rf ~/../var/www/production-project
+mv ~/production-project/build ~/../var/www/production-project/html
