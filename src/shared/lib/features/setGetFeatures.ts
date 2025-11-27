20a6bb62d557a9a1a81d371e325c87d15cd45934
gmtdn <failgim@gmail.com>
2025-11-27 20:34:21 +0300

    add feature flags

---
+++ src/shared/lib/features/setGetFeatures.ts
@@ -0,0 +1,14 @@
+import { FeatureFlags } from "@/shared/types/featureFlags";
+
+// Фичи в ходе сессии не меняются, их не обязательно делать реактивными
+let featureFlags: FeatureFlags;
+
+export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
+    if (newFeatureFlags) {
+        featureFlags = newFeatureFlags
+    }
+}
+
+export function getFeatureFlag(flag: keyof FeatureFlags) {
+    return featureFlags[flag];
+}
