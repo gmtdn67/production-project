0d2a42688a1f66a0f1ce55f4171a7e2909886648
gmtdn <failgim@gmail.com>
2026-01-08 14:44:24 +0300

    force update костыль

--- src/features/uiDesignSwitcher/ui/UiDesignSwitcher/UiDesignSwitcher.tsx
+++ src/features/uiDesignSwitcher/ui/UiDesignSwitcher/UiDesignSwitcher.tsx
@@ -9,6 +9,7 @@ import { getUserAuthData } from '@/entities/User';
 import { HStack } from '@/shared/ui/redesigned/Stack';
 import { Text } from '@/shared/ui/redesigned/Text';
 import { Skeleton } from '@/shared/ui/redesigned/Skeleton';
+import { useForceUpdate } from '@/shared/lib/render/forceUpdate';
 
 interface UiDesignSwitcherProps {
     className?: string;
@@ -21,6 +22,7 @@ export const UiDesignSwitcher = memo((props: UiDesignSwitcherProps) => {
     const dispatch = useAppDispatch();
     const authData = useSelector(getUserAuthData);
     const [isLoading, setIsLoading] = useState(false);
+    const forceUpdate = useForceUpdate();
 
     const items = [
         {
@@ -46,6 +48,7 @@ export const UiDesignSwitcher = memo((props: UiDesignSwitcherProps) => {
             );
         }
         setIsLoading(false)
+        forceUpdate()
     };
 
     return (
