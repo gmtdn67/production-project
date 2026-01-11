905ea6c0d9ccb43f4439150c123079984b9548d4
gmtdn <failgim@gmail.com>
2026-01-11 13:30:14 +0300

    add loading layout, fallback theme, default features

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
+import { LOCAL_STORAGE_LAST_DESIGN_KEY, USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
 import { UserSchema, User } from '../types/user';
 import { setFeatureFlags } from '@/shared/lib/features';
 import { saveJsonSettings } from '../services/saveJsonSettings';
@@ -14,13 +14,14 @@ export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        setAuthData: (state, action: PayloadAction<User>) => {
-            state.authData = action.payload;
-            setFeatureFlags(action.payload.features);
+        setAuthData: (state, {payload}: PayloadAction<User>) => {
+            state.authData = payload;
+            setFeatureFlags(payload.features);
+            localStorage.setItem(USER_LOCALSTORAGE_KEY, payload.id);
             localStorage.setItem(
-                USER_LOCALSTORAGE_KEY,
-                action.payload.id
-            )
+                LOCAL_STORAGE_LAST_DESIGN_KEY,
+                payload.features?.isAppRedesigned ? 'new' : 'old',
+            );
         },
         logout: (state) => {
             state.authData = undefined;
@@ -44,12 +45,9 @@ export const userSlice = createSlice({
                 state._inited = true;
             },
         );
-        builder.addCase(
-            initAuthData.rejected,
-            (state) => {
-                state._inited = true
-            },
-        );
+        builder.addCase(initAuthData.rejected, (state) => {
+            state._inited = true;
+        });
     },
 });
 
--- src/shared/lib/features/lib/setGetFeatures.ts
+++ src/shared/lib/features/lib/setGetFeatures.ts
@@ -1,7 +1,13 @@
+import { LOCAL_STORAGE_LAST_DESIGN_KEY } from "@/shared/const/localstorage";
 import { FeatureFlags } from "@/shared/types/featureFlags";
 
+const defaultFeatures: FeatureFlags = {
+    isAppRedesigned: localStorage.getItem(LOCAL_STORAGE_LAST_DESIGN_KEY) === 'new'
+}
 // Фичи в ходе сессии не меняются, их не обязательно делать реактивными
-let featureFlags: FeatureFlags = {};
+let featureFlags: FeatureFlags = {
+    ...defaultFeatures
+};
 
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
     if (newFeatureFlags) {
